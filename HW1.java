29c6935b6795dbe37689cc8efdd7bc341d1e7cf2
NguyenManh <22022521@vnu.edu.vn>
2023-03-12 17:53:14 +0700

    first commit

---
+++ HW1.java
@@ -0,0 +1,10 @@
+public class HW1{
+    public static void main(String[] args) {
+        System.out.println("Nguyen Van Manh"+'\t'+"22022521"+'\t'+"NguyenManhAI"+'\t'+"22022521@vnu.edu.vn.");
+        for(int i=9;i>=1;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall."+'\n');
+     }
+} 
